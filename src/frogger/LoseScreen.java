8c5486bf35bd0b8d46b0fa11cf559d6f7420d379
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-11-21 06:13:20 -0300

    add lose screen

---
+++ src/frogger/LoseScreen.java
@@ -0,0 +1,25 @@
+package frogger;
+
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLAutoDrawable;
+
+public class LoseScreen implements GLEventListener{
+    
+    @Override
+    public void init(GLAutoDrawable glad){
+    }
+    
+    @Override
+    public void dispose(GLAutoDrawable glad){}
+    
+    @Override
+    public void display(GLAutoDrawable glad){
+        GL2 gl = glad.getGL().getGL2();
+        gl.glClearColor(0, 1.0f, 0, 0);
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
+    }
+    
+    @Override
+    public void reshape(GLAutoDrawable glad, int i, int i1, int i2, int i3){}
+}
