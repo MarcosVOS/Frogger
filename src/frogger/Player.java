330de23d5c4937a485b0b43a7bf033f1e69c3a51
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-11-30 11:52:59 -0300

    adds animation to the frog's movement

--- src/frogger/Player.java
+++ src/frogger/Player.java
@@ -26,71 +26,81 @@ public class Player {
         this.stepX = 1.0f / numCols;
     }
 
-    public void moveLeft() {
+     public void moveLeft() {
         this.x -= stepX;
         float size = 0.09f;
         if (this.x - size < -1.0f) {
             this.x = -1.0f + size;
         }
-        this.rotationAngle = 180.0f;
+        this.rotationAngle = 90.0f;  
         sounds.playSound(sounds.getHopFrogger());
-
     }
 
     public void moveRight() {
         this.x += stepX;
-        float size = 0.09f; 
+        float size = 0.09f;
         if (this.x + size > 1.0f) {
             this.x = 1.0f - size;
         }
-        this.rotationAngle = 0.0f;
+        this.rotationAngle = 270.0f;
         sounds.playSound(sounds.getHopFrogger());
     }
 
     public void moveUp() {
         this.y += stepY;
-        float size = 0.09f; 
+        float size = 0.09f;
         if (this.y + size > 1.0f) {
             this.y = 1.0f - size;
         }
+
+        this.rotationAngle = 0.0f; 
         sounds.playSound(sounds.getHopFrogger());
     }
 
     public void moveDown() {
         this.y -= stepY;
-        float size = 0.09f; 
+        float size = 0.09f;
         if (this.y - size < -1.0f) {
             this.y = -1.0f + size;
         }
+        this.rotationAngle = 180.0f;  
         sounds.playSound(sounds.getHopFrogger());
     }
-    
-   public void draw(GL2 gl){
+
+    public void draw(GL2 gl) {
         gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         float size = 0.09f;
-       
+
         ImageLoader imageLoader = new ImageLoader(gl);
         Texture texture = imageLoader.getPlayerFroggerTexture();
-        
+
         texture.bind(gl);
         texture.enable(gl);
-        
+
         gl.glEnable(GL2.GL_BLEND);
         gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
-        
+
+        gl.glPushMatrix(); 
+
+        gl.glTranslatef(x, y, 0.0f);
+        gl.glRotatef(rotationAngle, 0.0f, 0.0f, 1.0f);
+        gl.glTranslatef(-x, -y, 0.0f);
+
         gl.glBegin(GL2.GL_QUADS);
-        gl.glTexCoord2f(0, 1); 
+        gl.glTexCoord2f(0, 1);
         gl.glVertex2f(x - size, y - size);
-        gl.glTexCoord2f(1, 1);  
-        gl.glVertex2f(x + size, y - size); 
-        gl.glTexCoord2f(1, 0); 
+        gl.glTexCoord2f(1, 1);
+        gl.glVertex2f(x + size, y - size);
+        gl.glTexCoord2f(1, 0);
         gl.glVertex2f(x + size, y + size);
-        gl.glTexCoord2f(0, 0); 
+        gl.glTexCoord2f(0, 0);
         gl.glVertex2f(x - size, y + size);
         gl.glEnd();
 
+        gl.glPopMatrix(); 
         texture.disable(gl);
-   }
+    }
+
             
    public float getX(){
        return this.x;
